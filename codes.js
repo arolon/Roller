87ead06d481c7c3d2334d60d06d2fb99a6dc72fb
Alvaro <afra.9622@gmail.com>
2019-05-30 21:29:22 -0500

    Updates
    Last Updates

--- codes.js
+++ codes.js
@@ -30,12 +30,14 @@ console.log("g esta en hola: " + count("Hola", 'g') + " veces");
   return newArray;
 }
 
-console.log("Here we'll see the result: " + filter([3, 5]));
+console.log("Aqui veremos el resultado: " + filter([3, 5]));
 console.log("Here we'll see the result: " + filter([3, 12, 45, 7]));
-console.log("Here we'll see the result: " + filter([3, 12, 45, 7, 88, -1, 10, 11, 9]));
+console.log("Here we'll see the result: " + filter([3, 12, 45, 7, 88, -1, 10, 11, 9, 100, 140, -50]));
 
 function hypotenuse (a, b){
-  return Math.sqrt(a*a + b*b);
+  var h;
+  h = Math.sqrt(a*a + b*b);
+  return h;
 }
 
 console.log("La hipotenusa de 3 y 4 es: " + hypotenuse(3,4));
